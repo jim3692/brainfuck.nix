1e124d3b6a33885569c115ee947c855047948e2e
jim3692 <jim3692@gmail.com>
2024-12-12 09:26:30 +0200

    move all io code

--- flake.nix
+++ flake.nix
@@ -17,25 +17,7 @@
           program = "${run}/bin/run";
         };
 
-      io = rec {
-        bytesToTextBin = pkgs.writeCBin "bytesToTextBin" (builtins.readFile ./io/bytes-to-text.c);
-        bytesToText = bytes: with pkgs;
-          let
-            input = builtins.toFile "input" (lib.concatMapStringsSep "\n" (b: toString b) bytes);
-            result = runCommand "bytesToText" { } "(echo '${toString (builtins.length bytes)}' ; cat '${input}') | ${bytesToTextBin}/bin/bytesToTextBin >$out";
-          in
-            builtins.readFile result;
-
-        textToBytesBin = pkgs.writeCBin "textToBytesBin" (builtins.readFile ./io/text-to-bytes.c);
-        textToBytes = text: with pkgs;
-          let
-            input = builtins.toFile "input" text;
-            result = runCommand "textToBytes" { } "(cat '${input}' ; echo -ne '\\0') | ${textToBytesBin}/bin/textToBytesBin >$out";
-            lines = lib.splitString "\n" (builtins.readFile result);
-            nonEmptyLines = builtins.filter (l: l != "") lines;
-          in
-            builtins.map (b: lib.toInt b) nonEmptyLines;
-      };
+      io = import ./io pkgs;
 
       brainfuck = import ./brainfuck.nix pkgs.lib;
     in {
